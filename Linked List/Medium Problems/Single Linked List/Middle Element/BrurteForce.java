19d1e9fdba28df6dd7d69883d1e7a9f05cd32f20
Srithan M.S <123616762+srithan09@users.noreply.github.com>
2025-08-12 09:41:54 +0530

    3sum

---
+++ Linked List/Medium Problems/Single Linked List/Middle Element/BrurteForce.java
@@ -0,0 +1,21 @@
+import java.util.LinkedList;
+
+public class BrurteForce {
+    public void MiddleLinkedList(LinkedList<Integer> list)
+    {
+        int size = list.size();
+        int mid = (size/2)+1;
+        Integer head = list.getFirst();
+        while () {
+            
+        }
+    }
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+    }
+}
---
+++ Problem-Arrays/Medium Level/3 Sum/Optimal3Sum.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Optimal3Sum {
+    public List<List<Integer>> threeSum(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
+        
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue; 
+            }
+            int j = i + 1;
+            int k = n - 1;
+            
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum > 0) {
+                    k--;
+                } else if (sum < 0) {
+                    j++;
+                } else {
+                    ans.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+                    k--;
+                    while (j < k && nums[j] == nums[j - 1]) j++;
+                    while (j < k && nums[k] == nums[k + 1]) k--;
+                }
+            }
+        }
+        return ans;
+    }
+    
+    public static void main(String[] args) {
+        Optimal3Sum obj = new Optimal3Sum();
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+        System.out.println(obj.threeSum(nums));
+    }
+}
